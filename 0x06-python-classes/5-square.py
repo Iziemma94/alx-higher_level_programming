711f73097ead2ec93c6ab2d9f8015a4f369c1c2c
root <taricarol632@>
2022-12-21 15:03:28 -0800

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,72 +1,42 @@
 #!/usr/bin/python3
-"""
-Module 5-square
-Defines class Square with private size and public area
-Can access and update size
-Can print to stdout the square using #'s
-"""
+"""Defines a class square"""
 
 
 class Square:
-    """
-    class Square definition
-
-    Args:
-        size (int): size of a side in square
-    
-    Functions:
-        __init__(self, size)
-        size(self)
-        size(self, value)
-        area(self)
-        print(self)
-    """
+    """Represents a square."""
 
     def __init__(self, size=0):
+        """Initializes a new square.
+        Args:
+        size(int):the size of the new square.
         """
-        Initializes square
-
-        Attributes:
-            size (int): defaults to 0 if none; don't use __size to call setter
-        """
-        self.size = size
+        self.__size = size
 
     @property
     def size(self):
-        """"
-        Getter
-
-        Return: size
-        """
+        """Return the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """
-        Setter
-
-        Args:
-            value: sets size to value if int and >= 0
-        """
-        if type(value) is not int:
+        """Sets the value of the size of the square"""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-    
-    def area(self):
-        """
-        Calculates area of square
+        self.__size = value
 
-        Returns:
-            area
-        """
-        return (self.__size)**2
+    def area(self):
+        """returns the current square area"""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """
-        Prints square with #'s
-        """
-        print("\n".join(["#" * self.__size for rows in range(self.__size)]))
+        """prints in stdout the square with the character #."""
+        if (self.__size == 0):
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
 
